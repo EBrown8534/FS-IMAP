07f1ca91bc8221494a202efdb6a8f1b600bff5aa
Elliott Brown <ebrown8534@gmail.com>
2019-09-03 10:50:54 -0400

    Fix spelling for `UidValidity`

--- EBrown.Imap.Server/Storage.fs
+++ EBrown.Imap.Server/Storage.fs
@@ -3,12 +3,12 @@ open EBrown.Imap.Core
 
 type Configuration =
     { Password : string
-      UidValididty : int
+      UidValidity : int
       NextUniqueId : int }
 let getConfig (f : string) =
     let lines = System.IO.Path.Combine(@"DemoFiles", f, @"Configuration.txt") |> System.IO.File.ReadAllLines
-    { Password = lines.[0]; UidValididty = lines.[1] |> int; NextUniqueId = lines.[2] |> int }
+    { Password = lines.[0]; UidValidity = lines.[1] |> int; NextUniqueId = lines.[2] |> int }
 let getStorageEvents () = 
     { Authenticate = (fun (u, p) -> if (u |> getConfig).Password = p then Some { Id = u } else None)
-      GetUidValidity = (fun u -> (u.Id |> getConfig).UidValididty)
+      GetUidValidity = (fun u -> (u.Id |> getConfig).UidValidity)
       GetNextUniqueId = (fun u -> (u.Id |> getConfig).NextUniqueId) }
